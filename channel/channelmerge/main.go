aabb076c6116af36f3e6e2abcee97fa53b355a18
root <root@localhost.localdomain>
2019-09-04 09:02:52 +0800

    modify

--- channel/channelmerge/main.go
+++ channel/channelmerge/main.go
@@ -10,6 +10,10 @@ func main() {
 	input2 := make(chan int)
 	output := make(chan int)
 
+	// golang 的 select 的功能和 select, poll, epoll 相似， 就是监听 IO 操作，当 IO 操作发生时，触发相应的动作。
+	// select 的代码形式和 switch 非常相似， 不过 select 的 case 里的操作语句只能是"IO操作"
+	// （不仅仅是取值<-channel，赋值channel<-也可以）， select 会一直等待等到某个 case 语句完成，
+	// 也就是等到成功从channel中读到数据. 则 select 语句结束
 	go func(in1, in2 <-chan int, out chan<- int) {
 		for {
 			select {
